00726f81439a0c6261f4a164b5186006b47ce9b5
M4 <56578014+m4github@users.noreply.github.com>
2021-10-23 09:47:05 +0330

    add main.c

--- src/main.c
+++ src/main.c
@@ -3,18 +3,36 @@
  * @author Mohadeseh_Forghani (m4ghaniofficial@gmail.com)
  * @brief main file to config the dhcp server with user data.
  * @version 0.1.0
- * @date 21 Oct 2021
+ * @date 23 Oct 2021
  *
  * @copyright Copyright (c) 2021
  *
  */
-#include <stdio.h>L
+#include <stdio.h>
 
 #include"dhcpd_conf.h"
 
-int main()
+int
+main()
 {
-struct variables config;
+  struct variable data;
 
-initMem()
+  initMem (&data);
+
+  setLoc (&data);
+
+  getSubnet (&data);
+
+  getnetMask (&data);
+
+  getRange (&data);
+
+  getGateway (&data);
+
+  getDNS (&data);
+
+  closeFile (&data);
+
+  freeMem (&data);
+
 }
